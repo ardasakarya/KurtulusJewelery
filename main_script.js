ae58fbcaad66f9262b52af3a5d7fcafdf854ccc0
ardasakarya <sakaryaarda@gmail.com>
2025-05-30 23:44:54 +0300

    header düzenlenmesi

--- main_script.js
+++ main_script.js
@@ -1,19 +1,21 @@
-
 document.addEventListener('DOMContentLoaded', function () {
   const mobileMenuButton = document.querySelector('.mobile-menu-button');
   const mobileMenu = document.querySelector('.mobile-menu');
   const closeMenuButton = document.querySelector('.close-menu-button');
+  const body = document.body;
 
   // Menü Aç
   mobileMenuButton.addEventListener('click', function () {
     mobileMenu.classList.remove('scale-y-0', 'opacity-0', 'pointer-events-none');
     mobileMenuButton.classList.add('hidden'); // Hamburger ikonunu gizle
+    body.classList.add('overflow-hidden');    // Sayfa scroll'unu engelle
   });
 
   // Menü Kapat (Çarpı Tuşu)
   closeMenuButton.addEventListener('click', function () {
     mobileMenu.classList.add('scale-y-0', 'opacity-0', 'pointer-events-none');
     mobileMenuButton.classList.remove('hidden'); // Hamburger ikonunu geri göster
+    body.classList.remove('overflow-hidden');    // Sayfa scroll'unu aç
   });
 
   // Menüdeki Linke Tıklanınca Menü Kapat
@@ -21,9 +23,11 @@ document.addEventListener('DOMContentLoaded', function () {
     link.addEventListener('click', () => {
       mobileMenu.classList.add('scale-y-0', 'opacity-0', 'pointer-events-none');
       mobileMenuButton.classList.remove('hidden'); // Hamburger ikonunu geri göster
+      body.classList.remove('overflow-hidden');    // Sayfa scroll'unu aç
     });
   });
 });
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -40,10 +44,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-
 const header = document.getElementById("site-header");
+const mobileMenu = document.querySelector(".mobile-menu");
 
+// Scroll olayını dinle
 window.addEventListener("scroll", () => {
+  // Mobil menü açıkken scroll işlemini atla
+  const isMenuOpen = !mobileMenu.classList.contains("scale-y-0");
+
+  if (isMenuOpen) return;
+
   if (window.scrollY > 50) {
     header.classList.add("bg-white/95", "shadow-lg", "backdrop-blur");
     header.classList.remove("bg-transparent");
@@ -64,6 +74,7 @@ window.addEventListener("scroll", () => {
     });
   }
 });
+
 
 
 
